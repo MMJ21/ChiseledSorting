0f71a53ffe821f2e9be81644b20a20a653eae331
Marcos Martínez Játiva <marcos.martinez-jativa@capgemini.com>
2024-05-21 09:35:24 +0200

    Sorting

---
+++ ChiseledSorting/MyObject.cs
@@ -0,0 +1,9 @@
+﻿namespace ChiseledSorting
+{
+    public class MyObject
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? CreationDateTime { get; set; }
+    }
+}
---
+++ ChiseledSortingNonAot/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChiseledSorting;
+
+var myObjects = new List<MyObject>();
+for (int i = 1; i <= 5000000; i++)
+{
+    myObjects.Add(new MyObject
+    {
+        Id = i,
+        Name = $"Object {i}",
+        CreationDateTime = DateTime.Now,
+    });
+}
+Console.WriteLine($"Total number of objects: {myObjects.Count}");
+Console.WriteLine($"First object on the list is {myObjects.First().Name}");
+Console.WriteLine($"Last object on the list is {myObjects.Last().Name}");
+Console.WriteLine("Reversing list...");
+
+var result = myObjects.OrderByDescending(o => o.Id).ToList();
+Console.WriteLine($"First object on the list is {result.First().Name}");
+Console.WriteLine($"Last object on the list is {result.Last().Name}");
